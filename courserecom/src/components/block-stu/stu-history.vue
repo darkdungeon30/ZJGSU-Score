b9f7b8d5fa4cd91821d19e51840e01ab58117922
xiaojia <12834440+small-leave@user.noreply.gitee.com>
2024-12-24 21:58:36 +0800

    实现了修改评分时修改add_time

--- courserecom/src/components/block-stu/stu-history.vue
+++ courserecom/src/components/block-stu/stu-history.vue
@@ -208,7 +208,7 @@ function submitRating() {
         homework: null,
         difficulty: null
       };
-
+      selectedCourse.value.add_time = formatDate(new Date());
       // 重新绘制雷达图
       updateRadarChart(selectedCourse.value); // 重新绘制雷达图
     } else {
@@ -266,6 +266,17 @@ function getRadarOption(labels, scores, seriesName, title) {
     }]
   };
 }
+// 格式化日期的方法
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 </script>
 <style scoped>
 .course-list-container {
